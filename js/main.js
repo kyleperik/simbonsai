e93f84653ebc8e1080b93d6086c063bdf924988d
Kyle Perik <kyle@periks.net>
2019-02-18 22:41:44 -0600

    make simple branch meshes

--- js/main.js
+++ js/main.js
@@ -24,10 +24,25 @@ document.body.appendChild( renderer.domElement );
 
 var material = new THREE.LineBasicMaterial( { color: 0x00ff00 } );
 
+// SIMPLE STRUCTURE FOR TESTING
+//  var structure = {
+//     length: 5,
+//     anglex: Math.PI * 3 / 2,
+//     angley: Math.PI / 2,
+//     nodes: [
+//         {
+//             length: 5,
+//             anglex: 0,
+//             angley: Math.PI * 3 / 2,
+//             nodes: [],
+//         }
+//     ],
+// };
+
 var structure = {
     length: 7,
     anglex: Math.PI * 3 / 2,
-    angley: 0,
+    angley: Math.PI / 5,
     nodes: [
         {
             length: 3,
@@ -36,13 +51,13 @@ var structure = {
             nodes: [
                 {
                     length: 3,
-                    anglex: Math.PI / 32,
+                    anglex: -Math.PI / 32,
                     angley: 0,
                     nodes: []
                 },
                 {
                     length: 3,
-                    anglex: Math.PI / 2,
+                    anglex: -Math.PI / 6,
                     angley: Math.PI / 2,
                     nodes: []
                 },
@@ -60,21 +75,21 @@ var structure = {
                     nodes: [
                         {
                             length: 3,
-                            anglex: Math.PI / 32,
+                            anglex: -Math.PI / 32,
                             angley: 0,
                             nodes: []
                         },
                         {
                             length: 3,
-                            anglex: -Math.PI / 5,
-                            angley: Math.PI / 4,
+                            anglex: -Math.PI / 3,
+                            angley: Math.PI / 3,
                             nodes: []
                         },
                     ]
                 },
                 {
                     length: 2,
-                    anglex: Math.PI / 32,
+                    anglex: -Math.PI / 32,
                     angley: 0,
                     nodes: [
                         {
@@ -85,7 +100,7 @@ var structure = {
                         },
                         {
                             length: 2,
-                            anglex: -Math.PI / 2,
+                            anglex: -Math.PI / 3,
                             angley: -Math.PI * 7 / 4,
                             nodes: []
                         }
@@ -96,16 +111,57 @@ var structure = {
     ]
 };
 
+function flat(l) {
+    return [].concat.apply([], l);
+}
+
+function range(n) {
+    return [...Array(n).keys()];
+}
+
+var resolution = 5;
+var width = .2;
+
 function renderStructure(node, baseVector, baseAnglex, baseAngley) {
     var newBaseAnglex = baseAnglex + node.anglex;
     var newBaseAngley = baseAngley + node.angley;
     var newBaseVector = new THREE.Vector3(0, 0, node.length);
-    newBaseVector.applyAxisAngle(new THREE.Vector3(0, 1, 0), newBaseAngley);
     newBaseVector.applyAxisAngle(new THREE.Vector3(1, 0, 0), newBaseAnglex);
+    newBaseVector.applyAxisAngle(new THREE.Vector3(0, 1, 0), newBaseAngley);
     newBaseVector.add(baseVector);
     var geometry = new THREE.Geometry();
-    geometry.vertices.push(baseVector, newBaseVector);
-    var segment = new THREE.Line(geometry, material);
+    // Get all the points needed for this
+    var points = flat(range(node.length + 1).map(
+        l => range(resolution).map(
+            r => {
+                var rel = new THREE.Vector3(width, 0, l);
+                rel.applyAxisAngle(new THREE.Vector3(0, 0, 1), r * Math.PI * 2 / resolution);
+                rel.applyAxisAngle(new THREE.Vector3(1, 0, 0), newBaseAnglex);
+                rel.applyAxisAngle(new THREE.Vector3(0, 1, 0), newBaseAngley);
+                return rel.add(baseVector);
+            }
+        )
+    ));
+    // Now triangulate!
+    var faces = flat(range(node.length).map(l => {
+        var base = l * resolution;
+        return flat(range(resolution).map(r => [
+            new THREE.Face3(
+                base + r,
+                base + (r + 1) % resolution,
+                base + r + resolution
+            ), new THREE.Face3(
+                base + (r + 1) % resolution,
+                base + (r + 1) % resolution + resolution,
+                base + r + resolution
+            )
+        ]));
+    }));
+    geometry.vertices = points;
+    geometry.faces = faces;
+    geometry.computeFaceNormals();
+    geometry.computeVertexNormals();
+    var segment = new THREE.Mesh(geometry, material);
     return [segment].concat([].concat.apply([], node.nodes.map(
         n => renderStructure(n, newBaseVector, newBaseAnglex, newBaseAngley)
     )));
@@ -118,7 +174,7 @@ scene.add(tree);
 
 function animate() {
     requestAnimationFrame( animate );
-    tree.rotation.y += 0.002;
+    tree.rotation.y += 0.004;
     renderer.render( scene, camera );
 }
 animate();
